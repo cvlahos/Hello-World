bc579271bd969add01c5a58d2596de3e5c0de0ba
cvlahos <cvlahos@users.noreply.github.com>
2016-09-07 18:42:17 -0400

    Add files via upload

---
+++ decisions/Decisions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Decisions : MonoBehaviour 
+{
+
+	public float disco;
+	// Use this for initialization
+	void Start () 
+	{
+		disco = 2.5f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	if (disco > 8.3f) 
+		{
+			Debug.Log ("Mr. V is the man!!");
+		}
+
+		else 
+		{
+
+			Debug.Log ("Disco is dead");
+		}
+
+		}
+
+}
---
+++ decisions/JetpackControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JetpackControl : MonoBehaviour 
+{
+	public Rigidbody sphereRB;
+
+	public int thrusterForce;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		int fuel;
+		fuel = 500;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Debug.Log (fuel);
+		//if the player holds the spacebar then the script will check if the jetpack has fuel
+		if (Input.GetKey(KeyCode.Space)) 
+		{
+			//if the fuel variable is greater than 0 (an empty tank) then the codeblock below will run
+			if (fuel > 0) 
+			{
+				// the below code will take control over the sphere's rigidbody and add a local force relative to the Y axis. The variable thrusterForce controlls how much force
+				sphereRB.AddRelativeForce (0,thrusterForce,0);
+				// while the force is applied fuel will be going down
+				fuel = fuel - 1;
+			}
+		}
+
+	}
+}
