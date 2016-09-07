ca48968152e92eacbddc4af9debbca541259f70d
cvlahos <cvlahos@users.noreply.github.com>
2016-09-07 18:55:01 -0400

    Add files via upload

---
+++ decisions/CubeControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeControl : MonoBehaviour 
+{
+	public Rigidbody cubeRB;
+
+	public int thrusterForce;
+
+	int fuel;
+
+	// Use this for initialization
+	void Start () 
+	{
+		fuel = 300;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Debug.Log (fuel);
+
+		if (fuel >= 0) 
+		{
+			cubeRB.AddForce (0,thrusterForce,0);
+			fuel = fuel - 1;
+		}
+
+
+	}
+}
