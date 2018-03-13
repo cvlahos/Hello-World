a02e8b1e1bb9bf0db35dbe8488c9b96a61658a2e
cvlahos <cvlahos@users.noreply.github.com>
2018-03-13 12:03:14 -0400

    Add files via upload

---
+++ Scripts/Key.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour 
+{
+	public GameManager gameManager;//Store a reference to the GameManager Class so we can change if the player has a key or not
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+
+	//OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
+	void OnTriggerEnter2D() 
+	{
+		//Debug.Log ("trigger collder activated");
+		gameManager.haveKey = true;
+		Destroy (gameObject); // this will destroy the game object this script is on - removing the key in this case
+	}
+	
+
+}
